67fd4df35fb0bdbf0997c24c8d75dc5eba7cbcae
liepaur <liepa.urbonaite@mif.stud.vu.lt>
2025-12-12 17:14:23 +0200

    Atnaujinta

---
+++ build/CMakeFiles/programa.dir/src/sum.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/programa.dir/src/sum.cpp.o: \
+  /Users/Liepa/Library/CloudStorage/OneDrive-Personal/Duomenų\ mokslas/2k.\ 2\ semestras/Objektinis\ programavimas/objektinio_git/3-oji-uzduotis/src/sum.cpp \
+  /Users/Liepa/Library/CloudStorage/OneDrive-Personal/Duomenų\ mokslas/2k.\ 2\ semestras/Objektinis\ programavimas/objektinio_git/3-oji-uzduotis/src/sum.h
